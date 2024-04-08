87626b87ca9ee79db33fc35675a29a93de752cdd
chenylnti <2374957923@qq.com>
2024-04-08 12:36:53 +0800

    登录页面

--- src/main.js
+++ src/main.js
@@ -1,14 +1,19 @@
+//前端工程的入口文件,类似于java的main方法
 import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 
 import App from './App.vue'
+//导入路由文件，按理说应该是 ./router/index.js，但是后缀是.js可以省略，进一步，后缀如果是index也可以省略
 import router from './router'
-
+//将App这个组建展示在index.html中的一个id为app的div中
+//即项目启动之后看到的都是app的内容
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
-
+app.use(ElementPlus)
 app.mount('#app')
---
+++ src/views/LoginView.vue
@@ -0,0 +1,61 @@
+<template>
+  <div>
+    <el-card style="max-width: 480px">
+    <template #header>
+      <div class="card-header">
+        <span>登录</span>
+      </div>
+    </template>
+    <div>
+      <table>
+        <tr>
+          <td>用户名</td>
+          <td><el-input
+    v-model="hr.username"
+    style="width: 240px"
+    placeholder="请输入用户名..."
+    clearable
+  /></td>
+        </tr>
+        <tr>
+          <td>密码</td>
+          <td><el-input
+    v-model="hr.password"
+    style="width: 240px"
+    type="password"
+    placeholder="请输入密码..."
+    show-password
+  /></td>
+        </tr>
+      </table>
+    </div>
+    <template #footer>
+      <div style="display: flex;justify-content: center;">
+        <el-button>重制</el-button>
+        <el-button type="primary" @click="loginHandle">登录</el-button>
+      </div>
+      
+    </template>
+  </el-card>
+  </div>
+</template>
+
+<script setup >
+import {reactive,toRefs} from "vue"
+const data=reactive({
+  hr:{
+    username: 'admin',
+    password: '123'
+  }
+})
+const {hr} = toRefs(data)
+
+
+function loginHandle(){
+  alert(hr.value.username);
+  alert(hr.value.password);
+}
+</script>
+<style scoped>
+
+</style>
